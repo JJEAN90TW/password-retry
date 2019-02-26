188bfa3c80d7d45b9b8d428c480e0e360cacce7b
JJEAN <jennyjean423@gmail.com>
2019-02-26 17:51:40 +0800

    adding more msgs

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
 password = '123456'
 t = 3 # chance left
 while t > 0:
+    t = t - 1
     pwd = input('please enter your password:')
     if pwd == password:
-        print('Log in!!')
+        print('Log in!!GOOD JOB!!')
         break # end
     else:
-        t = t - 1
-        print('ERROR!!', t, 'time(s) left!!')
+        print('ERROR!!')
+        if t > 0:
+            print( t, 'time(s) left!!')
+        if t == 0:
+            print('WRONG LA!! GET OUT!!')
